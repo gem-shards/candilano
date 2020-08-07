6246b95354d935e5e8008bb93a6ac0ca2bc14f14
VvanGemert <vincent@floorplanner.com>
2020-08-07 23:04:12 +0200

    Allowed role based tasks

--- src/lib/programs/restart.cr
+++ src/lib/programs/restart.cr
@@ -10,7 +10,7 @@ module Candilano
 
       def run
         task_group = Task::Group.new("restart:app", "restart application", @config)
-        task_group.tasks << Task.new(@config["restart_command"].to_s, false)
+        task_group.tasks << Task.new(@config["restart_command"].to_s, user_env: false)
         task_group.execute(@ssh)
       end
     end
--- src/lib/task.cr
+++ src/lib/task.cr
@@ -3,23 +3,22 @@ require "./task/*"
 module Candilano
   class Task
     property command : String
+    property role : String | Nil
     property user_env : Bool
     property dry_run : Bool
 
-    def initialize(command, user_env = true, dry_run = false)
+    def initialize(command, @user_env = true, @dry_run = false, @role = nil)
       @command = command
-      @user_env = user_env
-      @dry_run = dry_run
     end
 
     def execute(ssh)
       print "   " + @command.gsub("\n", "").colorize(:yellow).to_s + "\n"
-      ssh.execute(command, @user_env, dry_run)
+      ssh.execute(command, user_env: @user_env, dry_run: @dry_run, role: @role)
     end
 
     def execute_local(local)
       print ("   LOCAL: " + @command.gsub("\n", "")).colorize(:yellow).to_s + "\n"
-      local.execute(command, dry_run)
+      local.execute(command, dry_run: @dry_run)
     end
   end
 end
